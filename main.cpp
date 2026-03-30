515fa387621b8d2f7169a296bc214b7f3dbbd40c
Maxim <makstmainacc@outlook.com>
2026-03-30 18:35:17 +0500

    Declared primary functions of the program. Designed the first function of adding the new book with all properties. Also added limits and dynamic array.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,37 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <limits>
 using namespace std;
+
+const int MAX_BOOKS = 100;
+const int MAX_USERS = 50;
 
 struct Book {
   int bookid;
   string title;
   string author;
+  int year;
   bool isBorrowed;
+};
+
+struct Author {
+  string name;
+  int totalBooks; 
 };
 
 struct User {
   int userid;
   vector<int> borrowedBookIDs; 
 };
+
+void addbook();
+void viewbooks();
+void borrowbook();
+void returnbook();
+
+vector<Book> library;
 
 int main() {
   int choice;
@@ -27,7 +45,85 @@ int main() {
     <<endl<<"Enter your choice (1-5): ";
     cin>>choice;
 
+    switch (choice) {
+      case 1: 
+        addbook();
+      break;
+      case 2:
+        viewbooks();
+      break;
+      case 3:
+        borrowbook();
+      break;
+      case 4:
+        returnbook();
+      break;
+    }
+
   }while (choice != 5);
+  
+  cout<<"Exiting the program..."<<endl;
+  
 
   return 0;
+}
+
+
+
+void addbook(){
+  string inputline;
+
+  if (library.size() >= MAX_BOOKS){
+    cout<<"Library is full!"<<endl;
+    return;
+  }
+
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  cout<<"Enter the following: format: TITLE, AUTHOR, YEAR PUBLISHED: ";
+  getline(cin, inputline);
+
+  stringstream ss(inputline);
+  string bookTitle, authorName, year;
+  
+  getline(ss, bookTitle, ',');
+  getline(ss, authorName, ',');
+  getline(ss, year);
+  
+
+  bookTitle.erase(0, bookTitle.find_first_not_of(" "));
+  bookTitle.erase(bookTitle.find_last_not_of(" " +1 ));
+
+  authorName.erase(0, authorName.find_first_not_of(" "));
+  authorName.erase(authorName.find_last_not_of(" ") +1 );
+
+  year.erase(0, year.find_first_not_of(" "));
+  year.erase(year.find_last_not_of(" ") +1 );
+  
+  int yearPublished; //converts year from string to int
+
+  try {
+    yearPublished = stoi(year);
+  }catch (...){
+    cout<<"Invalid year input!"<<endl;
+    return;
+  }
+
+  Book newBook{
+    static_cast<int>(library.size() +1), bookTitle, authorName, yearPublished, false
+  };
+  library.push_back(newBook);
+
+  cout<<"Book added successfully!"<<endl;
+}
+
+void viewbooks() {
+    cout << "View Books not implemented yet." << endl;
+}
+
+void borrowbook() {
+    cout << "Borrow Book not implemented yet." << endl;
+}
+
+void returnbook() {
+    cout << "Return Book not implemented yet." << endl;
 }
